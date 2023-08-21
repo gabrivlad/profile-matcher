2f18b6fa6fc5dda7e84d88f0de4097903bd45020
gabriel-vlad <gabriel.vlad@vodafone.com>
2023-08-21 13:32:40 +0300

    add new exception

---
+++ src/main/java/com/example/profilematcher/exception/handler/SqlInitializationException.java
@@ -0,0 +1,7 @@
+package com.example.profilematcher.exception.handler;
+
+public class SqlInitializationException extends RuntimeException {
+    public SqlInitializationException(String message) {
+        super(message);
+    }
+}
